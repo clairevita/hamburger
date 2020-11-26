566733c4d9a8b4f3f3557ce7a366a3da228dc879
claire <clairej.vita@gmail.com>
2020-11-25 23:08:36 -0500

    finalized controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,25 +18,20 @@ router.get("/", function(req, res) {
     });
   });
 
-//   router.put("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
-  
-//     console.log("condition", condition);
-  
-//     cat.update(
-//       {
-//         sleepy: req.body.sleepy
-//       },
-//       condition,
-//       function(result) {
-//         if (result.changedRows === 0) {
-//           // If no rows were changed, then the ID must not exist, so 404
-//           return res.status(404).end();
-//         }
-//         res.status(200).end();
-  
-//       }
-//     );
-//   });
+  router.put("/api/burgers/:id", function(req, res) {
+    const condition = "id = " + req.params.id;
+    burger.updateOne(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          return res.status(404).end();
+        }
+        res.status(200).end();
+      }
+    );
+  });
 
 module.exports = router;
