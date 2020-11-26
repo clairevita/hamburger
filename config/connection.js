9c17301e4ed9d21c1febeae0e646164fffe0b2ed
claire <clairej.vita@gmail.com>
2020-11-25 21:02:48 -0500

    server fix

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Oakmage228!",
-  database: "burgers_db"
+
 });
 
 // Make connection.
