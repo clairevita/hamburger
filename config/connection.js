2a4c351963fe97e05061c931ebe8009530a17f25
claire <clairej.vita@gmail.com>
2020-11-25 21:09:02 -0500

    env

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require('dotenv').config();
 
 var connection = mysql.createConnection({
-
+  host: process.env.DB_HOST,//"localhost"
+  port: process.env.DB_PORT,//3306,
+  user: proces.env.DB_USER,//"root",
+  password: process.env.DB_PASS,//"Oakmage228!",
+  database: "burgers_db"
 });
 
 // Make connection.
