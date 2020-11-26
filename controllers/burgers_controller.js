396c83a99f40d621378abae416b391daaaafcb26
claire <clairej.vita@gmail.com>
2020-11-25 23:05:45 -0500

    post functionality

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -12,12 +12,11 @@ router.get("/", function(req, res) {
     });
   });
 
-//   router.post("/api/cats", function(req, res) {
-//     cat.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
-//       // Send back the ID of the new quote
-//       res.json({ id: result.insertId });
-//     });
-//   });
+  router.post("/api/burgers", function(req, res) {
+    burger.createOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+      res.json({ id: result.insertId });
+    });
+  });
 
 //   router.put("/api/cats/:id", function(req, res) {
 //     var condition = "id = " + req.params.id;
