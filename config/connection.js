894d282500279033865e61e6a13b44390decfbf4
claire <clairej.vita@gmail.com>
2020-11-26 20:27:53 -0500

    heroku

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@
 var mysql = require("mysql");
 require('dotenv').config();
 
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,//"localhost"
-  port: process.env.DB_PORT,//3306,
-  user: process.env.DB_USER,//"root",
-  password: process.env.DB_PASS,//"Oakmage228!",
-  database: "burgers_db"
-});
+  var connection = mysql.createConnection({
+    host: process.env.JAWSDB_HOST,
+    port: process.env.JAWSDB_PORT,
+    user: process.env.JAWSDB_USER,
+    password: process.env.JAWSDB_PASS,
+    database: JAWSDB_DB
+  });
 
 // Make connection.
 connection.connect(function(err) {
