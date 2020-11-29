de5738b56490a5b15fe57029ae76a69698a43a07
Claire Vita <clairej.vita@gmail.com>
2020-11-28 23:58:51 -0500

    connection variable

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require('dotenv').config();
-
+let connection;
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-  var connection = mysql.createConnection({
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASSW,
-    // database: "burgers_db"
-  });
+  // var connection = mysql.createConnection({
+  //   // host: process.env.DB_HOST,
+  //   // port: process.env.DB_PORT,
+  //   // user: process.env.DB_USER,
+  //   // password: process.env.DB_PASSW,
+  //   // database: "burgers_db"
+  // });
 // Make connection.
 connection.connect(function(err) {
   if (err) {
