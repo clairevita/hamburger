3650a67ed917c47216a7a40e518605ffbab1140c
Claire Vita <clairej.vita@gmail.com>
2020-11-28 23:54:56 -0500

    set static public folder

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+require('dotenv').config();
+
+app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
